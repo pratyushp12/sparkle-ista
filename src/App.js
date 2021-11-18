0fb537fbb34c3398eade67ea28a90f21e945f6d3
Pratyush Praveen <pratyush1296@gmail.com>
2021-11-18 23:55:05 +0530

    Menu-Item Directory Component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Homepage} from './components/Homepage.component';
+import { Homepage } from './pages/Homepage';
 
 function App() {
   return (
     <div>
-    <Homepage />
+      <Homepage />
     </div>
   );
 }
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Directory from "../components/directory/directory.components";
+import './Homepage.styles.css'
+
+export const Homepage = ()=>{
+    return (
+    <div className='homepage'>
+    <Directory />
+    </div>
+    );
+}
