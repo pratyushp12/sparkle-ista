dd6fc1e150ec978c70863f2b5d37613419a4ccae
Pratyush Praveen <pratyush1296@gmail.com>
2021-11-19 13:54:28 +0530

    Menu-Item component style completed

--- src/components/directory/directory.components.jsx
+++ src/components/directory/directory.components.jsx
@@ -9,33 +9,33 @@ class Directory extends Component{
             sections:[
                 {
                     title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+                    imageUrl: 'https://www.wholesaler-hats.com/upfile/product/custom-embroidery-snapback-hats,design-snapback-hats_4.jpg',
                     id: 1,
                     linkUrl: 'shop/hats'
                   },
                   {
                     title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+                    imageUrl: 'https://www.stelladesign.eu/wp-content/uploads/2019/09/how-to-furnish-a-clothing-store.jpg',
                     id: 2,
                     linkUrl: 'shop/jackets'
                   },
                   {
                     title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+                    imageUrl: 'https://footwearnews.com/wp-content/uploads/2015/08/jordy-geller-sneakers-collection-22.jpg',
                     id: 3,
                     linkUrl: 'shop/sneakers'
                   },
                   {
                     title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+                    imageUrl: 'http://seaislandtrading.com/wp-content/gallery/clothing/clothing12.jpg',
                     size: 'large',
                     id: 4,
                     linkUrl: 'shop/womens'
                   },
                   {
                     title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                    size: 'large',
+                    imageUrl: 'https://www.outfittrends.com/wp-content/uploads/2014/06/men-summer-style-ideas.jpg',
+                    size : 'large',
                     id: 5,
                     linkUrl: 'shop/mens'
                   }
@@ -47,8 +47,8 @@ class Directory extends Component{
         return (
             <div className='directory-menu'>
             {
-                this.state.sections.map(({title,id,imageUrl}) => 
-                    <MenuItem key={id} title={title} />
+                this.state.sections.map(({title,id,imageUrl, size}) => 
+                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
                 )
             }
             </div>
