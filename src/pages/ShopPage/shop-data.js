951481fd47c2b605be0eae3d06b9847249118217
Pratyush Praveen <pratyush1296@gmail.com>
2021-11-27 13:50:31 +0530

    modified Shop-Data page

--- src/pages/ShopPage/shop-data.js
+++ src/pages/ShopPage/shop-data.js
@@ -73,26 +73,26 @@ const SHOP_DATA = [
         },
         {
           id: 2,
-          name: 'Adidas Yeezy',
-          imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
+          name: 'Nike Air Max',
+          imageUrl: 'https://www.prodirectselect.com/productimages/V3_1_Gallery_1/229745_Gallery_1_0780375.jpg',
           price: 280
         },
         {
           id: 3,
-          name: 'Black Converse',
-          imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
+          name: 'Jordan Air',
+          imageUrl: 'http://sneakerz.co/wp-content/uploads/2018/04/26220005_1936985466329526_5088479686448340497_n.jpg',
           price: 110
         },
         {
           id: 4,
-          name: 'Nike White AirForce',
-          imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
+          name: 'Converse All Star',
+          imageUrl: 'https://n2.sdlcdn.com/imgs/a/5/1/Converse-White-Casual-Shoes-SDL529851253-2-74736.jpg',
           price: 160
         },
         {
           id: 5,
-          name: 'Nike Red High Tops',
-          imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
+          name: 'Balenciaga Hoka',
+          imageUrl: 'https://i.pinimg.com/736x/91/de/07/91de071fc9786248b76a7936c417fd0e.jpg',
           price: 160
         },
         {
@@ -122,26 +122,26 @@ const SHOP_DATA = [
       items: [
         {
           id: 1,
-          name: 'Black Jean Shearling',
-          imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
+          name: 'Leather Jacket',
+          imageUrl: 'https://happygentleman.com/wp-content/uploads/2019/11/aviatrix-danny-mens-leather-jacket-tan-4.jpg',
           price: 125
         },
         {
           id: 2,
-          name: 'Blue Jean Jacket',
-          imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
+          name: 'Linen Jacket',
+          imageUrl: 'https://tornadovintage.com/wp-content/uploads/2018/03/20180327_134246-e1522249403269.jpg',
           price: 90
         },
         {
           id: 3,
-          name: 'Grey Jean Jacket',
-          imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
+          name: 'Over Coat',
+          imageUrl: 'http://drfashion.co.uk/eshop/6904-thickbox_default/mens-winter-over-coat-3-4-long-jacket-suede-finish-tweed-cashmere-4895-jackets-and-coats.jpg',
           price: 90
         },
         {
           id: 4,
-          name: 'Brown Shearling',
-          imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
+          name: 'Sports Jacket',
+          imageUrl: 'https://www.revzilla.com/product_images/0036/2720/agv_sport_solare_jacket.jpg',
           price: 165
         },
         {
@@ -159,26 +159,26 @@ const SHOP_DATA = [
       items: [
         {
           id: 1,
-          name: 'Blue Tanktop',
-          imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
+          name: 'Gown',
+          imageUrl: 'https://i5.walmartimages.com/asr/92de899c-e646-4ee8-922e-c8bd3d90cfab_1.78239a078aac4ac0929bb6485a8948d1.jpeg',
           price: 25
         },
         {
           id: 2,
-          name: 'Floral Blouse',
-          imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
+          name: 'Nike T-Shirt',
+          imageUrl: 'https://www.tennisnuts.com/images/product/full/829747-100-PHSFH001-2000.jpg',
           price: 20
         },
         {
           id: 3,
-          name: 'Floral Dress',
-          imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
+          name: 'Ripped Jeans',
+          imageUrl: 'https://s3-eu-west-1.amazonaws.com/images.linnlive.com/fd6c4c1fc0c305a6e41fb6c823555ada/8c5886f2-0cc2-415d-a055-f04dab52ac48.jpg',
           price: 80
         },
         {
           id: 4,
-          name: 'Red Dots Dress',
-          imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
+          name: 'Dark Lilac Gown',
+          imageUrl: 'https://www.bridesmaidca.ca/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/H/T/HT23-03/HT23-Princess-Strapless-Long-Dark-Lilac-Tulle-Flower-Girl-Dress-34.jpg',
           price: 80
         },
         {
@@ -208,26 +208,26 @@ const SHOP_DATA = [
       items: [
         {
           id: 1,
-          name: 'Camo Down Vest',
-          imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
+          name: 'Sweatshirt',
+          imageUrl: 'https://image.sportsmansguide.com/adimgs/l/1/121077i12_ts.jpg',
           price: 325
         },
         {
           id: 2,
-          name: 'Floral T-shirt',
-          imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
+          name: 'T-shirt',
+          imageUrl: 'http://drfashion.co.uk/eshop/6491-thickbox_default/mens-casual-polo-t-shirt-uk-size-thin-soft-cotton-blue-badge-clips-2095-t-shirts-polos.jpg',
           price: 20
         },
         {
           id: 3,
-          name: 'Black & White Longsleeve',
-          imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+          name: 'Full Sleeves T-Shirt',
+          imageUrl: 'https://n4.sdlcdn.com/imgs/h/4/n/Nike-Red-Full-Sleeve-T-SDL910370548-1-b987c.png',
           price: 25
         },
         {
           id: 4,
-          name: 'Pink T-shirt',
-          imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+          name: 'Jeans',
+          imageUrl: 'https://pngimg.com/uploads/jeans/jeans_PNG5767.png',
           price: 25
         },
         {
