a5affc2ca62be103ebed18288f4206e0f5003824
Pratyush Praveen <pratyush1296@gmail.com>
2022-01-02 20:30:45 +0530

    Implemented Sign-up component with user data saving to firestore

--- src/pages/SignIn-and-SignUp/signIn-and-signUp.components.jsx
+++ src/pages/SignIn-and-SignUp/signIn-and-signUp.components.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.componenet';
+import SignUp from '../../components/sign-up/sign-up.component';
+import './signIn-and-signUp.styles.scss'
 
 export const SignInSignOut = ()=>{
     return(
-        <div>
+        <div className='sign-in-and-sign-up'>
             <SignIn />
+            <SignUp />
         </div>
     )
 }
