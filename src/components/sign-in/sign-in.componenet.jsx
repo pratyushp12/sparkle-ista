2b34504aeea023c85b42075fc3f671af05afc116
Pratyush Praveen <pratyush1296@gmail.com>
2022-01-03 01:40:55 +0530

    Redux setup completed

--- src/components/sign-in/sign-in.componenet.jsx
+++ src/components/sign-in/sign-in.componenet.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { auth, createUserProfileData, signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import CustomButton from '../custom-button/custom-button.component';
 import FormInput from '../form-input/form-input.component';
 import './sign-in.component.styles.scss'
