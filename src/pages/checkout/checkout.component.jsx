4dcc0969e0cd3caad4f5d9c1d6c2ecc4241d37fe
Pratyush Praveen <pratyush1296@gmail.com>
2022-01-08 11:18:42 +0530

    Implemented Remove/Delete cartItem on the checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import CheckoutItem from '../../components/checkout/checkout.component';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors'
 
 
